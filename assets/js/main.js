ba666714ac7329045d7eb20d25690e82b49a05c2
Tux87 <84878111+Tux87@users.noreply.github.com>
2022-03-19 23:53:28 +0100

    Added title mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">Christophe Génot</span>' +
 				'</div>'
 			)
 				.appendTo($body);
